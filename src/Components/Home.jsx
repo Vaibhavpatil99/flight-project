2cd19ab985c3ae7312443a4553bbe8dd5c7f3d8a
Vaibhavpatil99 <patilvb1999@gmail.com>
2022-10-08 19:56:39 +0530

    filter Data working

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 const Home = () => {
   const [FromList, setFromList] = useState();
   const [ToList, setToList] = useState();
+  console.log(FromList)
   var data = [
     {
       "airline": "novo",
@@ -17,9 +18,18 @@ const Home = () => {
               "name": "VQ-901",
               "plane": "ATR725",
               "departureDate": "2020-04-26T07:00",
-              "origin": "DAC",
+              "origin": "MUMBAI",
+              "arrivalDate": "2022-04-26T07:55",
+              "destination": "DELHI",
+              "stops": 0
+            },
+            {
+              "name": "VQ-902",
+              "plane": "ATR725",
+              "departureDate": "2022-05-26T07:00",
+              "origin": "MUMBAI",
               "arrivalDate": "2020-04-26T07:55",
-              "destination": "CGP",
+              "destination": "DELHI",
               "stops": 0
             },
             {
@@ -72,60 +82,99 @@ const Home = () => {
   ]
   // console.log(FromList, ToList)
   return (
-    <div className='px-32'>
-      <div className=' shadow rounded-xl my-20'>
-        <div className='flex relative bottom-10 border mx-20 shadow-lg rounded-xl px-12'>
-          <div className=' border-b-2 border-blue-300 p-2 mx-8'>
-            <img src={plane} alt="" className='h-8' />
-            <h2 className='text-xl'>Plane</h2>
-          </div>
-          <div className=' p-2 mx-8'>
-            {/* <img src={plane} alt="" className='h-8' /> */}
-            <h2 className='text-xl'>Buses</h2>
-          </div>
-          <div className=' p-2 mx-8'>
-            {/* <img src={plane} alt="" className='h-8' /> */}
-            <h2 className='text-xl'>Hotels</h2>
-          </div>
-          <div className=' p-2 mx-8'>
-            {/* <img src={plane} alt="" className='h-8' /> */}
-            <h2 className='text-xl'>HomeStays</h2>
-          </div>
-          <div className=' p-2 mx-8'>
-            {/* <img src={plane} alt="" className='h-8' /> */}
-            <h2 className='text-xl'>Trains</h2>
-          </div>
-          <div className=' p-2 mx-8'>
-            {/* <img src={plane} alt="" className='h-8' /> */}
-            <h2 className='text-xl'>Cabs</h2>
-          </div>
-          <div className=' p-2 mx-8'>
-            {/* <img src={plane} alt="" className='h-8' /> */}
-            <h2 className='text-xl'>Cabs</h2>
-          </div>
+    <div>
+      <div className='py-20 bg-gradient-to-b from-sky-900 to-indigo-900'>
+        <div className='px-32 '>
+          <div className='bg-white shadow rounded-xl'>
+            <div className='flex relative bottom-10 border mx-20 shadow-lg rounded-xl px-12 bg-white'>
+              <div className=' border-b-2 border-blue-300 p-2 mx-8'>
+                <img src={plane} alt="" className='h-8' />
+                <h2 className='text-xl'>Plane</h2>
+              </div>
+              <div className=' p-2 mx-8'>
+                {/* <img src={plane} alt="" className='h-8' /> */}
+                <h2 className='text-xl'>Buses</h2>
+              </div>
+              <div className=' p-2 mx-8'>
+                {/* <img src={plane} alt="" className='h-8' /> */}
+                <h2 className='text-xl'>Hotels</h2>
+              </div>
+              <div className=' p-2 mx-8'>
+                {/* <img src={plane} alt="" className='h-8' /> */}
+                <h2 className='text-xl'>HomeStays</h2>
+              </div>
+              <div className=' p-2 mx-8'>
+                {/* <img src={plane} alt="" className='h-8' /> */}
+                <h2 className='text-xl'>Trains</h2>
+              </div>
+              <div className=' p-2 mx-8'>
+                {/* <img src={plane} alt="" className='h-8' /> */}
+                <h2 className='text-xl'>Cabs</h2>
+              </div>
+              <div className=' p-2 mx-8'>
+                {/* <img src={plane} alt="" className='h-8' /> */}
+                <h2 className='text-xl'>Cabs</h2>
+              </div>
+            </div>
 
+            <div className='flex'>
+              <div className='border hover:bg-blue-100 duration-300 py-4 px-4 w-full'>
+                <h2 className='text-xl font-semibold'>FROM</h2>
+                <select className='text-3xl font-bold bg-transparent' onChange={(e) => { setFromList(e.target.value); }} name="" id="">
+                <option className='text-lg' value="">Select</option>
+                  {cities.map((e) => (
+                    <option className='text-lg' value={e}>{e}</option>
+                  ))}
+                </select>
+              </div>
+              <div className='border hover:bg-blue-100 duration-300 py-4 px-4 w-full'>
+                <h2 className='text-xl font-semibold'>TO</h2>
+                <select className='text-3xl font-bold bg-transparent' onChange={(e) => { setToList(e.target.value); }} name="" id="">
+                <option className='text-lg' value="">Select</option>
+                  {cities.map((e) => (
+                    <option className='text-lg' value={e}>{e}</option>
+                  ))}
+                </select>
+              </div>
+              <div className='border hover:bg-blue-100 duration-300 py-4 px-4 w-full'>
+                <h2 className='text-xl font-semibold'>Date</h2>
+                <input className='text-3xl font-bold bg-transparent' type="date" />
+              </div>
+              <div className='border hover:bg-blue-100 duration-300  py-4 px-4 w-full'>
+                <h2 className='text-xl font-semibold'>Select a Fare Type</h2>
+                <select className='text-3xl font-bold bg-transparent' onChange={(e) => { setToList(e.target.value); }} name="" id="">
+                  <option className='text-lg' value="">Regular Fares</option>
+                  <option className='text-lg' value="">Student Fares</option>
+                  <option className='text-lg' value="">Armed Forces Fares</option>
+                  <option className='text-lg' value="">Senior Citizen Fares</option>
+                  <option className='text-lg' value="">Doctor & nurses Fares</option>
+                </select>
+              </div>
+            </div>
+            <div>
+              <h2 className='text-center py-4'>Book International and Domestic Flights</h2>
+            </div>
+            <div className='flex justify-center items-center relative top-5'>
+              <button className=' rounded-full bg-gradient-to-r from-blue-900 to-blue-400 px-12 py-1 text-white text-2xl '>
+                Search
+              </button>
+            </div>
+          </div>
         </div>
-        <div className='flex'>
-          <div className='border hover:bg-blue-100 duration-300 py-4 px-4'>
-            <h2 className='text-xl font-semibold'>FROM</h2>
-            <select className='text-3xl font-bold bg-transparent' onChange={(e) => { setFromList(e.target.value); }} name="" id="">
-              {cities.map((e) => (
-                <option className='text-lg' value={e}>{e}</option>
-              ))}
-            </select>
-          </div>
-          <div className='border hover:bg-blue-100 duration-300  py-4 px-4'>
-            <h2 className='text-xl font-semibold'>TO</h2>
-            <select className='text-3xl font-bold bg-transparent' onChange={(e) => { setToList(e.target.value); }} name="" id="">
-              {cities.map((e) => (
-                <option className='text-lg' value={e}>{e}</option>
-              ))}
-            </select>
-          </div>
-          <div className='border hover:bg-blue-100 duration-300  py-4 px-4'>
-            <h2 className='text-xl font-semibold'>Date</h2>
-            <input className='text-3xl font-bold bg-transparent' type="date" />
-          </div>
+      </div>
+      <div>
+        <div className='border'>
+          <h2 className='text-xl font-semibold'>Flight Result</h2>
+          {data[0].flight.map((e) => (
+            <div>
+                {e.segments.filter((filter)=>(filter.origin == FromList&& filter.destination == ToList )).map((data) => (
+                  <div>
+                  <div value="">{data.origin}</div>
+                  <div value="">{data.name}</div>
+                  </div>
+                ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
